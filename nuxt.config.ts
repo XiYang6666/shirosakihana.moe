05b514a254bbfa2029ee8627b5c557d1c5223e1a
XiYang6666 <1782356858@qq.com>
2025-03-30 09:33:47 +0800

    :sparkles: use sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/scripts'],
-  css: ["public/style.css"]
+  css: ["assets/style.scss"]
 })
