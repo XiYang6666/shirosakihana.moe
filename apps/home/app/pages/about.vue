9e26ed21791705005bb7792b7ef82bc99ebf5e3c
XiYang6666 <1782356858@qq.com>
2026-02-17 17:32:06 +0800

    :bug: fix target

--- apps/home/app/pages/about.vue
+++ apps/home/app/pages/about.vue
@@ -2,42 +2,46 @@
   <PostBase title="关于">
     <p class="text-zinc-800">
       一个简单的白咲花粉丝站，由
-      <NuxtLink
-        href="https://github.com/XiYang6666"
-        class="underline font-bold"
-      >
+      <NuxtLink href="https://xiyang6666.top" target="_blank">
         @XiYang6666
       </NuxtLink>
-      运营
+      运营.
+      <br />
+      <NuxtLink
+        href="https://github.com/XiYang6666/shirosakihana.moe"
+        target="_blank"
+      >
+        <i class="fa-brands fa-github"></i> Github 仓库
+      </NuxtLink>
     </p>
 
     <p class="mt-4">图片素材来源:</p>
     <ul class="mt-2 pl-6">
       <li>
         <NuxtLink
-          class="underline"
           href="https://www.bilibili.com/bangumi/play/ss26291"
+          target="_blank"
         >
           番剧截图
         </NuxtLink>
       </li>
       <li>
         <NuxtLink
-          class="underline"
           href="https://gelbooru.com/index.php?page=post&s=list&tags=shirosaki_hana"
+          target="_blank"
         >
           Gelbooru
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink class="underline" href="https://space.bilibili.com/457053240">
+        <NuxtLink href="https://space.bilibili.com/457053240" target="_blank">
           Bilibili(@帕喵ネコ)
         </NuxtLink>
       </li>
       <li>
         <NuxtLink
-          class="underline"
           href="https://github.com/CompVis/stable-diffusion"
+          target="_blank"
         >
           Stable Diffusion
         </NuxtLink>
